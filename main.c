0b0d474e5b94adfa231d8dcebdc559fdf8163b07
yekyu94 <yekyu94@naver.com>
2018-05-14 18:46:28 +0900

    Fin~!

---
+++ main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    char data[1024] = {0};
+    char encode[1024] = {0};
+    int divisor[] = {1,0,0,0,0,0,1,1,1};
+    printf("CRC-8 Generator\n");
+    printf("7bit ASCII code\n");
+    printf("Divisor : x^8+x^2+x+1 (100000111)\n\n");
+    printf("  Data\t: ");
+    
+    scanf("%s", data);
+    
+    for(int i=0; i<strlen(data); i++){
+        int tmp = data[i];
+        for(int j=0; j<8; j++){
+            int k=(i+1)*8;
+            encode[k-j-1] = tmp % 2;
+            tmp /= 2;
+        }
+    }
+    
+    printf("\n  BinD\t:");
+    for(int i=0; i<strlen(data)*8; i++){
+        if(i%8 == 0) printf(" ");
+        printf("%d", encode[i]);
+    }
+    
+    for(int i=0; i<strlen(data)*8; i++){
+        if(encode[i] == 1){
+            for(int j=0; j<9; j++){
+                encode[i+j] = encode[i+j]^divisor[j];
+            }
+        }
+    }
+    
+    printf("\n  CRC\t: ");
+    for(int i=0; i<8; i++){
+        int tmp = (int)strlen(data)*8;
+        printf("%d", encode[tmp+i]);
+    }
+    printf("\n\n");
+    
+    return 0;
+}
